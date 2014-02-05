c451a522483e0f72c5ab763b5c9714dab51b194c
Nathan Fearnley <nfearnley@gmail.com>
2014-02-04 22:50:40 -0500

    Cleaned up code by moving split into seperate function, and changed backgrounds of new layers from transparent to white

--- transsplit.py
+++ transsplit.py
@@ -8,31 +8,30 @@ import math
 import time
 from gimpfu import *
 
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
 def transsplit(img, layer):
     pdb.gimp_image_undo_group_start(img)
     
-    # Create white layer
-    white_layer = pdb.gimp_layer_copy(layer, False)
-    pdb.gimp_image_insert_layer(img, white_layer, None, -1)
-    pdb.gimp_item_set_name(white_layer, "White")
-    pdb.gimp_message("Test")
-    pdb.gimp_image_select_color(img, CHANNEL_OP_REPLACE, white_layer, (255, 255, 255))
-    pdb.gimp_context_set_foreground((0, 0, 0))
-    pdb.gimp_edit_fill(white_layer, FOREGROUND_FILL)
-    pdb.gimp_selection_invert(img)
-    pdb.gimp_edit_clear(white_layer)
-    
-    # Create black layer
-    black_layer = pdb.gimp_layer_copy(layer, False)
-    pdb.gimp_image_insert_layer(img, black_layer, None, -1)
-    pdb.gimp_item_set_name(white_layer, "Black")
-    pdb.gimp_image_select_color(img, CHANNEL_OP_REPLACE, black_layer, (0, 0, 0))
-    pdb.gimp_selection_invert(img)
-    pdb.gimp_edit_clear(black_layer)
+    # Create seperate white and black layers
+    splitlayer(img, layer, WHITE, "White")
+    splitlayer(img, layer, BLACK, "Black")
 
     pdb.gimp_selection_clear(img)
     pdb.gimp_image_undo_group_end(img)
 
+def splitlayer(img, layer, color, name):
+    pdb.gimp_context_set_foreground(WHITE)
+    pdb.gimp_context_set_foreground(BLACK)
+    new_layer = pdb.gimp_layer_copy(layer, False)
+    pdb.gimp_image_insert_layer(img, new_layer, None, -1)
+    pdb.gimp_item_set_name(new_layer, name)
+    pdb.gimp_image_select_color(img, CHANNEL_OP_REPLACE, new_layer, color)
+    pdb.gimp_edit_fill(new_layer, FOREGROUND_FILL)
+    pdb.gimp_selection_invert(img)
+    pdb.gimp_edit_fill(new_layer, BACKGROUND_FILL)
+    
 register(
     "transsplit",
     "Split a black and white image into separate layers for Pebble transparency",
